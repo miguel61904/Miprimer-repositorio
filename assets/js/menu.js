a99de5a4a775d7bc55607597d473fe393e34b3d0
Miguelon <miguelon1230.mf@gmail.com>
2021-07-07 17:39:12 -0500

    elmine archivos y renove la ágina

---
+++ assets/js/menu.js
@@ -0,0 +1,7 @@
+const contact = document.querySelector('#contacto');
+contact.addEventListener('click', () =>{
+    alert("esta es mi información:\nCelular: 3154741259\nCorreo: miguelon1230.mf@gmail.com");
+})
+
+
+
